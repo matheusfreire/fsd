793ddaf93e1eebd50b3fff0df825af9ebe221eda
Arthur Del Esposte+Matheus Freire <arthurmde@yahoo.com.br>
2013-06-20 19:56:07 -0300

    Inicio Serviço Django BD

---
+++ Django Services/domain/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from calendar import monthrange
+import re
+import logging
+import os
+import datetime
+
+class User(models.Model):
+	
+	rb = models.IntegerField(primary_key = True)
+	name = models.CharField(max_length = 40)
+	email = models.CharField(max_length = 40, null = True)
+
+class Description(models.Model):
+
+	isbn = models.IntegerField(primary_key = True)
+	name = models.CharField(max_length = 100)
+	author = models.CharField(max_length = 40)
+	ano = models.DateField(null = True)
+
+class Book(models.Model):
+
+	description = models.ForeignKey(Description)
+	collection_id = models.IntegerField(primary_key = True)
+	status = models.BooleanField(default = False)
+
+class Loan(models.Model):
+
+	user = models.ForeignKey(User)
+	book = models.ForeignKey(Book)
+	begin = models.DateField()
+	end = models.DateField()
+	received = models.DateField()
+	penalty = models.DecimalField(max_digits = 10, decimal_places = 2, default = 0.0)
